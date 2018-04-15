d978c90c1c923d1c205d8bc3471e8d776c2c2c81
LiSongPeng <1442364096@qq.com>
2018-04-15 22:28:14 +0800

    添加配置

---
+++ src/main/java/com/solar/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.solar.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author LiHuiBo
+ */
+@Controller
+@Scope("prototype")
+public class UserController {
+}
---
+++ src/main/java/com/solar/utils/UUIDGenerator.java
@@ -0,0 +1,12 @@
+package com.solar.utils;
+
+import java.util.UUID;
+
+/**
+ * @author LiHuiBo
+ */
+public class UUIDGenerator {
+    public static String getUUID() {
+        return UUID.randomUUID().toString();
+    }
+}
