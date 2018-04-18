f31482a34b337037fd07db397d8d1aba8c1e72c1
LiSongPeng <1442364096@qq.com>
2018-04-18 12:46:00 +0800

    修改mapper.xml文件

--- src/test/java/com/solar/service/UserServiceTest.java
+++ src/test/java/com/solar/service/UserServiceTest.java
@@ -1,8 +1,10 @@
 package com.solar.service;
 
+import com.github.pagehelper.PageInfo;
 import com.solar.BaseJunitTest;
 import com.solar.entity.User;
 import com.solar.service.interfaces.UserService;
+import com.solar.utils.UUIDGenerator;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,44 +21,96 @@ public class UserServiceTest extends BaseJunitTest {
         String password = "123456789";
         User user = userService.login(phoneOrEmail, password);
         Assert.assertNotNull(user);
+        phoneOrEmail = "3423432423";
+        user = userService.login(phoneOrEmail, password);
+        Assert.assertNull(user);
     }
 
-    public void testFindPassword(String phoneOrEmail, String identify) {
-
+    @Test
+    public void testFindPassword() {
+        String phoneOrEmail = "15510841744";
+        String identify = "124214214124214214";
+        String password = userService.findPassword(phoneOrEmail, identify);
+        Assert.assertEquals("123456789", password);
+        phoneOrEmail = "234234324";
+        password = userService.findPassword(phoneOrEmail, identify);
+        Assert.assertNull(password);
     }
 
-    public void testRegister(User user) {
-
+    @Test
+    public void testRegister() {
+        User user = new User();
+        user.setName("sssss");
+        user.setEmail("23423423422@qq.com");
+        user.setGender(1);
+        user.setId(UUIDGenerator.getUUID());
+        user.setHeadImg("sdfsdfsdf.png");
+        user.setAlipay("sdfsdfsdsss.png");
+        user.setIdentify("324444444234234");
+        user.setLocation("sdfsdfdsfdsfsdf");
+        user.setPassword("12322423423");
+        user.setPhone("234543543543");
+        Assert.assertTrue(userService.register(user));
     }
 
-    public void testLikeComment(String commentId) {
-
+    @Test
+    public void testLikeComment() {
+        Assert.assertTrue(userService.likeComment("sdfdsfdssfs"));
     }
 
-    public void testDisklikeComment(String commentId) {
-
+    @Test
+    public void testDisklikeComment() {
+        Assert.assertTrue(userService.disklikeComment("sdfdsfdssfs"));
     }
 
-    public void testGetUsers(int pageNumber, int pageSize) {
-
+    @Test
+    public void testGetUsers() {
+        int pageNumber = 1;
+        int pageSize = 5;
+        PageInfo<User> list = userService.getUsers(pageNumber, pageSize);
+        Assert.assertTrue(list.getList().size() > 0);
     }
 
-    public void testGetUserByEmail(String email) {
-
+    @Test
+    public void testGetUserByEmail() {
+        String email = "15510841744";
+        User user = userService.getUserByEmail(email);
+        Assert.assertNull(user);
+        email = "1424242424@qq.com";
+        user = userService.getUserByEmail(email);
+        Assert.assertNotNull(user);
     }
 
-    public void testGetUserByPhone(String phone) {
-
+    @Test
+    public void testGetUserByPhone() {
+        String phone = "15510841744";
+        User user = userService.getUserByPhone(phone);
+        Assert.assertNotNull(user);
+        phone = "1424242424@qq.com";
+        user = userService.getUserByPhone(phone);
+        Assert.assertNull(user);
     }
 
-    public void testGetUserById(String id) {
-
+    @Test
+    public void testGetUserById() {
+        String id = "234234324234";
+        User user = userService.getUserById(id);
+        Assert.assertNotNull(user);
+        id = "1424242424@qq.com";
+        user = userService.getUserById(id);
+        Assert.assertNull(user);
     }
 
-    public void testUpdateUser(String id, String location,
-                               String email, String phone,
-                               String password, String headImg) {
-
+    @Test
+    public void testUpdateUser() {
+        String id = "22222222222";
+        String location = "sssssssssss";
+        String email = "23432@ss.com";
+        String phone = "43543543534";
+        String password = "4354sdfsd";
+        String headImg = "ssss.png";
+        boolean result = userService.updateUser(id, location, email, phone, password, headImg);
+        Assert.assertTrue(result);
     }
 
     @Autowired
